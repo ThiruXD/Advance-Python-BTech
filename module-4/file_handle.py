ec4f13fce644acc4ddad52cbe2c0003db0e9afcd
ThiruXD <thiruxd@gmail.com>
2026-08-20 23:26:17 +0530

    PermissionError & FileExistsError added

--- module-4/file_handle.py
+++ module-4/file_handle.py
@@ -6,3 +6,7 @@ try:
             print(line) 
 except FileNotFoundError:
     print(f"Error: The file '{filename}' was not found.")
+except PermissionError:
+    print("Enble premission to read or write.") # use this cmd to change 'chmod a-r comming-soon.txt' 
+except FileExistsError:
+    print("File Exists Error")
